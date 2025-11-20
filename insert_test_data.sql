cfb1be5d25c5d56a0399e92c04af283d341f1923
Weronika-Podgorska <weronikapodgorska10@gmail.com>
2025-11-20 23:15:46 +0000

    adding user

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,6 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99), ('Six of Crows', 13.00), ('Pride and Prejudice', 23.99) ;
+
+INSERT INTO users (first_name, last_name, email, username, hashed_password)
+VALUES ('Gold', 'Smith', 'gold@example.com', 'gold', '$2b$10$TeADRF6zfJ9OAoWUbf7EmuS3mUGVDdUhosjSSPbNdn8KPRDrm55UO');
